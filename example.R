ab60a4c0c407f0a463814324bfe6984a19e26f94
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-13 17:57:07 +0100

    dont normalise and add seed

--- example.R
+++ example.R
@@ -6,11 +6,13 @@ data <- dyntoy::generate_dataset(
   id = "specific_example/paga_tree",
   num_cells = 99,
   num_features = 101,
-  model = "tree"
+  model = "tree",
+  normalise = TRUE
 )
 
 # add method specific args (if needed)
 data$params <- list()
+data$seed <- 1
 
 # write example dataset to file
 file <- commandArgs(trailingOnly = TRUE)[[1]]
